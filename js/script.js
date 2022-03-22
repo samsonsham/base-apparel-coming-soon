4bcfa68a653685cdfe4b4940471630c719c80559
Samson Sham <samsonsham@gmail.com>
2022-03-22 20:29:39 +0000

    fix empty input submit issue

--- js/script.js
+++ js/script.js
@@ -1,8 +1,11 @@
-const showErrMsg = () => {
+const showErrMsg = (msg) => {
   const elemMsg = document.querySelector('.err-msg');
   const elemBtn = document.querySelector('.email__btn-submit');
+  const elemInput = document.querySelector('.email__input');
   elemMsg.classList.add('active');
   elemBtn.classList.add('error');
+  elemInput.style.border = '0.1rem solid hsl(0, 93%, 68%)';
+  elemMsg.innerHTML = msg;
 };
 
 const submitForm = () => {
@@ -12,11 +15,16 @@ const submitForm = () => {
 const formSubmitHandler = (e) => {
   e.preventDefault();
 
+  const emptyMsg = 'Email cannot be empty';
+  const errMsg = 'Please provide a valid email';
   const elemInput = document.querySelector('.email__input');
+
   // Validate email by HTML5 input type email
-  if (elemInput.matches(':valid')) {
+  if (elemInput.value === '') {
+    showErrMsg(emptyMsg);
+  } else if (elemInput.matches(':valid')) {
     submitForm();
   } else {
-    showErrMsg();
+    showErrMsg(errMsg);
   }
 };
