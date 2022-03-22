de99e91aa8731fc7926ce1eab12b0cdae5932fcf
Samson Sham <samsonsham@gmail.com>
2022-03-22 22:23:59 +0000

    improve submit button action

--- js/script.js
+++ js/script.js
@@ -1,14 +1,24 @@
-const showErrMsg = (msg) => {
+const showErrMsg = (isShowMsg, msg) => {
   const elemMsg = document.querySelector('.err-msg');
   const elemBtn = document.querySelector('.email__btn-submit');
   const elemInput = document.querySelector('.email__input');
-  elemMsg.classList.add('active');
-  elemBtn.classList.add('error');
-  elemInput.style.border = '0.1rem solid hsl(0, 93%, 68%)';
-  elemMsg.innerHTML = msg;
+
+  if (isShowMsg) {
+    elemMsg.classList.add('active');
+    elemBtn.classList.add('error');
+    elemInput.style.border = '0.1rem solid hsl(0, 93%, 68%)';
+    elemMsg.innerHTML = msg;
+  } else {
+    elemMsg.classList.remove('active');
+    elemBtn.classList.remove('error');
+    elemInput.style.border = '0.01rem solid hsl(0, 36%, 70%)';
+    elemMsg.innerHTML = msg;
+    elemInput.value = '';
+  }
 };
 
 const submitForm = () => {
+  showErrMsg(false, '');
   alert('Email Address submitted!');
 };
 
@@ -21,10 +31,10 @@ const formSubmitHandler = (e) => {
 
   // Validate email by HTML5 input type email
   if (elemInput.value === '') {
-    showErrMsg(msgEmpty);
+    showErrMsg(true, msgEmpty);
   } else if (elemInput.matches(':valid')) {
     submitForm();
   } else {
-    showErrMsg(msgInvalid);
+    showErrMsg(true, msgInvalid);
   }
 };
